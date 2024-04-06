9889c1862e0924cb3c6830bdfc3414e6ef506ede
J_W shin1.abe <shin1.abe@nifty.com>
2024-04-06 12:53:05 +0900

    Add files via upload

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------
 FFaudioConverter
-Copyright (C) 2018-2019  Elias Martin (Bleuzen)
+Copyright (C) 2018-2019  Bleuzen
 https://github.com/Bleuzen/FFaudioConverter
 supgesu@gmail.com
 
@@ -73,7 +73,7 @@ void MainWindow::processCommandLine(QStringList positionalArguments) {
     foreach (const QString &path, positionalArguments) {
         addFromPath(path);
     }
-    showTable();
+    showTable(true);
 }
 
 void MainWindow::dragEnterEvent(QDragEnterEvent *event)
@@ -88,7 +88,7 @@ void MainWindow::dragEnterEvent(QDragEnterEvent *event)
 void MainWindow::dropEvent(QDropEvent *event)
 {
     // Display file table if it is still hidden
-    showTable();
+    showTable(true);
 
     // Handle drop
     QList<QUrl> droppedUrls = event->mimeData()->urls();
@@ -101,6 +101,7 @@ void MainWindow::dropEvent(QDropEvent *event)
 }
 
 void MainWindow::addFromPath(QString localPath) {
+    if (Util::isNullOrEmpty(localPath)) return;
     QFileInfo fileInfo(localPath);
     if(fileInfo.isFile()) {
     // file
@@ -171,19 +172,19 @@ void MainWindow::setConvertItemStatus(QModelIndex index, FFmpegTask::ConvertStat
 {
     if(status == FFmpegTask::ConvertStatus::Pending) {
         model->setData(index, tr("Pending"));
-        model->setData(index, QColor(Qt::darkYellow), Qt::BackgroundRole);
+        model->setData(index, QColor(Qt::yellow), Qt::BackgroundRole);
     } else if(status == FFmpegTask::ConvertStatus::Converting) {
         model->setData(index, tr("Converting"));
         model->setData(index, QColor(Qt::darkYellow), Qt::BackgroundRole);
     } else if(status == FFmpegTask::ConvertStatus::Done) {
         model->setData(index, tr("Done"));
-        model->setData(index, QColor(Qt::darkGreen), Qt::BackgroundRole);
+        model->setData(index, QColor(Qt::green), Qt::BackgroundRole);
     } else if(status == FFmpegTask::ConvertStatus::Failed) {
         model->setData(index, tr("Failed"));
-        model->setData(index, QColor(Qt::darkRed), Qt::BackgroundRole);
+        model->setData(index, QColor(Qt::red), Qt::BackgroundRole);
     } else if(status == FFmpegTask::ConvertStatus::Skipped) {
         model->setData(index, tr("Skipped"));
-        model->setData(index, QColor(Qt::darkBlue), Qt::BackgroundRole);
+        model->setData(index, QColor(Qt::blue), Qt::BackgroundRole);
     }
 }
 
@@ -197,11 +198,6 @@ void MainWindow::removeTableRows(QList<int> rows)
           prev = current;
        }
     }
-}
-
-void MainWindow::resetTableModel()
-{
-    model->removeRows(0, model->rowCount());
 }
 
 void MainWindow::convertItem(int id, QString file)
@@ -271,10 +267,14 @@ void MainWindow::on_tableView_customContextMenuRequested(const QPoint &pos)
     menu->popup(ui->tableView->viewport()->mapToGlobal(pos));
 }
 
-void MainWindow::showTable() {
-    if(!ui->tableView->isVisible()) {
-        ui->tableView->setVisible(true);  // Display file table
+void MainWindow::showTable(bool e) {
+    if (e && !ui->tableView->isVisible()) {
+        ui->tableView->setVisible(true);  // Show file table
         ui->label_HelpMessage->setVisible(false);  // Hide help message
+    } else if (!e && ui->tableView->isVisible()) {
+        model->removeRows(0, model->rowCount());  // Clear file table
+        ui->tableView->setVisible(false);  // Hide file table
+        ui->label_HelpMessage->setVisible(true);  // Show help message
     }
 }
 
@@ -336,7 +336,7 @@ void MainWindow::on_pushButton_Cancel_clicked()
 
 void MainWindow::on_pushButton_Clear_clicked()
 {
-    resetTableModel();
+    showTable(false);
 }
 
 void MainWindow::on_pushButton_Settings_clicked()
